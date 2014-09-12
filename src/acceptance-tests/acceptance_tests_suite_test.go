a60dba7ea13dd501876d4750d63b9420a98b6888
Alex Suraci <asuraci@pivotallabs.com>
2014-09-11 17:22:10 -0700

    add initial acceptance tests
    also started turning the release into the $GOPATH; added submodules for
    all acceptance test dependencies

---
+++ src/acceptance-tests/acceptance_tests_suite_test.go
@@ -0,0 +1,33 @@
+package acceptance_test
+
+import (
+  "testing"
+  "os"
+
+  "github.com/cloudfoundry-incubator/garden/warden"
+  "github.com/cloudfoundry-incubator/garden/client"
+  "github.com/cloudfoundry-incubator/garden/client/connection"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var wardenClient warden.Client
+
+var _ = BeforeSuite(func() {
+  wardenNetwork := os.Getenv("WARDEN_NETWORK")
+  Ω(wardenNetwork).ShouldNot(BeEmpty(), "$WARDEN_NETWORK must be set")
+
+  wardenAddr := os.Getenv("WARDEN_ADDR")
+  Ω(wardenAddr).ShouldNot(BeEmpty(), "$WARDEN_ADDR must be set")
+
+  wardenClient = client.New(connection.New(wardenNetwork, wardenAddr))
+
+  err := wardenClient.Ping()
+  Ω(err).ShouldNot(HaveOccurred(), "Warden is not pingable.")
+})
+
+func TestAcceptance(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AcceptanceTests Suite")
+}
