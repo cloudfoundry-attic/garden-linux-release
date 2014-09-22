f9e673fcd63f2b21ac7e4286568dee150c45e0e1
Glyn Normington <gnormington@pivotal.io>
2014-09-22 12:17:45 +0100

    Rename garden/warden to garden/api
    Signed-off-by: Steve Powell <spowell@pivotal.io>

--- src/acceptance-tests/acceptance_tests_suite_test.go
+++ src/acceptance-tests/acceptance_tests_suite_test.go
@@ -1,30 +1,30 @@
 package acceptance_test
 
 import (
-  "testing"
-  "os"
+	"os"
+	"testing"
 
-  "github.com/cloudfoundry-incubator/garden/warden"
-  "github.com/cloudfoundry-incubator/garden/client"
-  "github.com/cloudfoundry-incubator/garden/client/connection"
+	"github.com/cloudfoundry-incubator/garden/api"
+	"github.com/cloudfoundry-incubator/garden/client"
+	"github.com/cloudfoundry-incubator/garden/client/connection"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var wardenClient warden.Client
+var wardenClient api.Client
 
 var _ = BeforeSuite(func() {
-  wardenNetwork := os.Getenv("WARDEN_NETWORK")
-  Ω(wardenNetwork).ShouldNot(BeEmpty(), "$WARDEN_NETWORK must be set")
+	wardenNetwork := os.Getenv("WARDEN_NETWORK")
+	Ω(wardenNetwork).ShouldNot(BeEmpty(), "$WARDEN_NETWORK must be set")
 
-  wardenAddr := os.Getenv("WARDEN_ADDR")
-  Ω(wardenAddr).ShouldNot(BeEmpty(), "$WARDEN_ADDR must be set")
+	wardenAddr := os.Getenv("WARDEN_ADDR")
+	Ω(wardenAddr).ShouldNot(BeEmpty(), "$WARDEN_ADDR must be set")
 
-  wardenClient = client.New(connection.New(wardenNetwork, wardenAddr))
+	wardenClient = client.New(connection.New(wardenNetwork, wardenAddr))
 
-  err := wardenClient.Ping()
-  Ω(err).ShouldNot(HaveOccurred(), "Warden is not pingable.")
+	err := wardenClient.Ping()
+	Ω(err).ShouldNot(HaveOccurred(), "Warden is not pingable.")
 })
 
 func TestAcceptance(t *testing.T) {
